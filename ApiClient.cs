b9f35d1ab93df453bce0a64466f7c43fe239db55
Ricardo <ricardorzan@gmail.com>
2021-05-22 11:17:00 -0500

    Stage

--- ApiClient.cs
+++ ApiClient.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Windows;
 using Employex.Models;
 using Newtonsoft.Json;
 using RestSharp;
@@ -36,7 +37,7 @@ namespace Employex
             return firstUser;
         }*/
 
-        RestClient client = new RestClient("http://localhost:8080/ricardorzan/Employex/1.0.0");
+        readonly RestClient client = new RestClient("http://localhost:8080/ricardorzan/Employex/1.0.0");
 
         public dynamic GetFirstJobOffer()
         {
@@ -79,7 +80,99 @@ namespace Employex
                 //var result = JsonConvert.DeserializeObject<String>(response.Content);
                 return true;
             else
+            {
+                switch (response.StatusCode)
+                {
+                    case System.Net.HttpStatusCode.Continue:
+                        break;
+                    case System.Net.HttpStatusCode.SwitchingProtocols:
+                        break;
+                    case System.Net.HttpStatusCode.OK:
+                        break;
+                    case System.Net.HttpStatusCode.Created:
+                        break;
+                    case System.Net.HttpStatusCode.Accepted:
+                        break;
+                    case System.Net.HttpStatusCode.NonAuthoritativeInformation:
+                        break;
+                    case System.Net.HttpStatusCode.NoContent:
+                        break;
+                    case System.Net.HttpStatusCode.ResetContent:
+                        break;
+                    case System.Net.HttpStatusCode.PartialContent:
+                        break;
+                    case System.Net.HttpStatusCode.Ambiguous:
+                        break;
+                    case System.Net.HttpStatusCode.Moved:
+                        break;
+                    case System.Net.HttpStatusCode.Redirect:
+                        break;
+                    case System.Net.HttpStatusCode.RedirectMethod:
+                        break;
+                    case System.Net.HttpStatusCode.NotModified:
+                        break;
+                    case System.Net.HttpStatusCode.UseProxy:
+                        break;
+                    case System.Net.HttpStatusCode.Unused:
+                        break;
+                    case System.Net.HttpStatusCode.TemporaryRedirect:
+                        break;
+                    case System.Net.HttpStatusCode.BadRequest:
+                        break;
+                    case System.Net.HttpStatusCode.Unauthorized:
+                        MessageBox.Show("Error al ingresar las credenciales");
+                        break;
+                    case System.Net.HttpStatusCode.PaymentRequired:
+                        break;
+                    case System.Net.HttpStatusCode.Forbidden:
+                        break;
+                    case System.Net.HttpStatusCode.NotFound:
+                        break;
+                    case System.Net.HttpStatusCode.MethodNotAllowed:
+                        break;
+                    case System.Net.HttpStatusCode.NotAcceptable:
+                        break;
+                    case System.Net.HttpStatusCode.ProxyAuthenticationRequired:
+                        break;
+                    case System.Net.HttpStatusCode.RequestTimeout:
+                        break;
+                    case System.Net.HttpStatusCode.Conflict:
+                        break;
+                    case System.Net.HttpStatusCode.Gone:
+                        break;
+                    case System.Net.HttpStatusCode.LengthRequired:
+                        break;
+                    case System.Net.HttpStatusCode.PreconditionFailed:
+                        break;
+                    case System.Net.HttpStatusCode.RequestEntityTooLarge:
+                        break;
+                    case System.Net.HttpStatusCode.RequestUriTooLong:
+                        break;
+                    case System.Net.HttpStatusCode.UnsupportedMediaType:
+                        break;
+                    case System.Net.HttpStatusCode.RequestedRangeNotSatisfiable:
+                        break;
+                    case System.Net.HttpStatusCode.ExpectationFailed:
+                        break;
+                    case System.Net.HttpStatusCode.UpgradeRequired:
+                        break;
+                    case System.Net.HttpStatusCode.InternalServerError:
+                        break;
+                    case System.Net.HttpStatusCode.NotImplemented:
+                        break;
+                    case System.Net.HttpStatusCode.BadGateway:
+                        break;
+                    case System.Net.HttpStatusCode.ServiceUnavailable:
+                        break;
+                    case System.Net.HttpStatusCode.GatewayTimeout:
+                        break;
+                    case System.Net.HttpStatusCode.HttpVersionNotSupported:
+                        break;
+                    default:
+                        break;
+                }
                 return false;
+            }
         }
     }
 }
--- View/Login.xaml.cs
+++ View/Login.xaml.cs
@@ -20,20 +20,12 @@ namespace Employex.View
 
         private void LoginButton_Clicked(object sender, RoutedEventArgs e)
         {
-            if (TryToLogIn(UserTextBox.Text, PasswordTextBox.Password)) {
+            ApiClient apiClient = new ApiClient();
+            if (apiClient.LogIn(UserTextBox.Text, PasswordTextBox.Password)) {
                 var mainWindow = (MainWindow)Application.Current.MainWindow;
                 mainWindow?.ChangeView(new Home());
                 return;
             }
-        }
-
-        private bool TryToLogIn(string text, string password)
-        {
-            ApiClient apiClient = new ApiClient();
-            if (apiClient.LogIn(text, password))
-                return true;
-            else
-                return false;
         }
 
         private void RegisterButton_Clicked(object sender, RoutedEventArgs e)
