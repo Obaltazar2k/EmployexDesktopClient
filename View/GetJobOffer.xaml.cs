5cbabc635cf4f1c22365cf0b9114ddd7f15ff6ec
Ricardo <ricardorzan@gmail.com>
2021-05-29 16:40:58 -0500

    Cambios en la interfaz de GetJobOffers

---
+++ View/GetJobOffer.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+
+namespace Employex.View
+{
+    /// <summary>
+    /// Lógica de interacción para UserControl1.xaml
+    /// </summary>
+    public partial class GetJobOffer : UserControl
+    {
+        public GetJobOffer()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public string getJobOfferJob { get; set; }
+        public string getJobOfferUsername { get; set; }
+        public string getJobOfferJobCategory { get; set; }
+        public string getJobOfferDescription { get; set; }
+        public string getJobOfferLocation { get; set; }
+    }
+}
